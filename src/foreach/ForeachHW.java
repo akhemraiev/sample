8db43496ca857e19e163cdd25e0a6812b90b9c01
Your Name <akhemraiev@luxoft.com>
2019-04-14 20:09:27 +0300

    foreach

---
+++ src/foreach/ForeachHW.java
@@ -0,0 +1,58 @@
+package foreach;
+
+public class ForeachHW {
+
+    public static void main(String[] args) {
+        /*
+        * 100000
+        * 010000
+        * 001000
+        * 000100
+        * 000010
+        * 000001
+        * */
+
+        /*
+        * 00001
+        * 00010
+        * 00100
+        * 01000
+        * 10000
+        * */
+
+        /*
+        * 10001
+        * 01010
+        * 00100
+        * 01010
+        * 10001
+        * */
+
+        /*
+        * X0X0X
+        * 0X0X0
+        * X0X0X
+        * 0X0X0
+        * X0X0X
+        * */
+
+        /*
+        * xxxxx
+        * x   x
+        * x   x
+        * x   x
+        * xxxxx
+        * */
+
+
+        /*
+        * x
+        * xx
+        * xxx
+        * xxxx
+        * xxxxx
+        * xxxxxx
+        * xxxxxxx
+        * */
+    }
+}
